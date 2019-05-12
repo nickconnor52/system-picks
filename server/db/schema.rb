b2a183ec53c52282c649ace25c6edb7576539ca6
Nick Connor <nickconnor52@gmail.com>
2019-05-11 23:59:09 -0400

    Update Schema to add created at for teams

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190512003048) do
+ActiveRecord::Schema.define(version: 20190512033324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,8 @@ ActiveRecord::Schema.define(version: 20190512003048) do
     t.text "location"
     t.text "name"
     t.text "nickname"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
