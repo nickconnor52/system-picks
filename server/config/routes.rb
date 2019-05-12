9884cde370d9327aecc1b1018fe178d2f9455e26
Nick Connor <nickconnor52@gmail.com>
2019-05-11 23:58:51 -0400

    Add teams to a resource route

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :matchups
   resources :users
+  resources :teams
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
