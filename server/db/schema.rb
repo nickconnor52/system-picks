feb58264303557787fb18b414b06d2bcbf35febf
Nick Connor <nickconnor52@gmail.com>
2019-05-11 21:22:07 -0400

    Update the schema to match the current database structure

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,11 +10,79 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190512003048) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "matchups", primary_key: "matchup_id", id: :text, force: :cascade do |t|
+    t.text "away_team_id"
+    t.text "correct_pick"
+    t.text "date"
+    t.boolean "deleted"
+    t.text "home_team_id"
+    t.text "note"
+    t.text "score.away_team"
+    t.text "score.home_team"
+    t.integer "season"
+    t.text "system_spread"
+    t.text "time"
+    t.text "vegas_spread"
+    t.integer "week"
+    t.text "spread_history"
   end
 
+  create_table "schedules", primary_key: "schedule_id", id: :text, force: :cascade do |t|
+    t.text "away_team.abbreviation"
+    t.text "away_team.city"
+    t.integer "away_team.api_id"
+    t.text "away_team.name"
+    t.text "date"
+    t.text "delayed_or_postponed_reason"
+    t.text "home_team.abbreviation"
+    t.text "home_team.city"
+    t.integer "home_team.api_id"
+    t.text "home_team.name"
+    t.integer "api_id"
+    t.text "location"
+    t.text "original_date"
+    t.text "original_time"
+    t.text "schedule_status"
+    t.integer "season"
+    t.text "time"
+    t.integer "week"
+  end
+
+  create_table "stats", primary_key: "stat_id", id: :text, force: :cascade do |t|
+    t.float "def_3rd_per"
+    t.float "def_LOS_drive"
+    t.float "def_pass_yds_game"
+    t.float "def_pts_game"
+    t.float "def_pts_rz"
+    t.float "def_RZA_game"
+    t.float "def_rush_yds_game"
+    t.integer "give_take_diff"
+    t.float "off_3rd_pct"
+    t.float "off_LOS_drive"
+    t.float "off_pass_yds_game"
+    t.float "off_pts_game"
+    t.float "off_pts_rz"
+    t.float "off_RZA_game"
+    t.float "off_rush_yds_game"
+    t.integer "season"
+    t.text "team_id", null: false
+    t.integer "week"
+  end
+
+  create_table "teams", primary_key: "team_id", id: :text, force: :cascade do |t|
+    t.integer "bye_week"
+    t.float "home_field_advantage"
+    t.text "location"
+    t.text "name"
+    t.text "nickname"
+  end
+
+  add_foreign_key "matchups", "teams", column: "away_team_id", primary_key: "team_id", name: "matchups_away_team_fkey"
+  add_foreign_key "matchups", "teams", column: "home_team_id", primary_key: "team_id", name: "matchups_home_team_fkey"
+  add_foreign_key "stats", "teams", primary_key: "team_id", name: "stats_team_fkey"
 end
