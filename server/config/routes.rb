30ef159c1313dc4a011da30f7377afc99caf94e1
Nick Connor <nickconnor52@gmail.com>
2019-05-25 16:12:03 -0400

    Add correct routing for teams to the api prefix

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  scope path: 'api' do
+    resources :teams
+  end
+
   resources :matchups
   resources :users
-  resources :teams
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
