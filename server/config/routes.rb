6aa41565e5c6ba8fc929fffd4e3d87bf327fbf6a
Nick Connor <nickconnor52@gmail.com>
2020-09-06 20:42:59 -0500

    light housekeeping

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
     resources :users
 
-  # Authentication Routes
+    # Authentication Routes
     post 'refresh', controller: :refresh, action: :create
     post 'signin', controller: :signin, action: :create
     post 'signup', controller: :signup, action: :create
